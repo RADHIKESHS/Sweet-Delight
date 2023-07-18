06330567655fd88d069c083ae35311f9ebbe305c
unknown <rahulmk.94802@gmail.com>
2023-07-18 21:52:47 +0530

    Entity and Repository created

--- SweetsDelight_BK/src/main/java/com/sweetsdelight_bk/Model/Product.java
+++ SweetsDelight_BK/src/main/java/com/sweetsdelight_bk/Model/Product.java
@@ -1,6 +1,10 @@
 package com.sweetsdelight_bk.Model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,5 +21,11 @@ public class Product {
 	private Double price;
 	private String description;
 	private boolean available;
+
+	@ManyToOne
+	@JoinColumn(name = "category_Id")
 	private Category category;
+
+	@OneToOne(mappedBy = "product", cascade = CascadeType.ALL)
+	private Admin admin;
 }
--- SweetsDelight_BK/src/main/java/com/sweetsdelight_bk/Model/Users.java
+++ SweetsDelight_BK/src/main/java/com/sweetsdelight_bk/Model/Users.java
@@ -1,9 +1,11 @@
 package com.sweetsdelight_bk.Model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,22 +20,15 @@ public class Users {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long userId;
+	
 	@Size(min = 5, max = 15, message = "Username should be greater than 5 and less than 15")
 	private String username;
+	
 	@Size(min = 8, message = "Password should be greater than 8")
 	private String password;
 	private String confirmPassword;
 	private String role;
 
-	public Users(
-			@Size(min = 5, max = 15, message = "Username should be greater than 5 and less than 15") String username,
-			@Size(min = 8, message = "Password should be greater than 8") String password, String confirmPassword,
-			String role) {
-		super();
-		this.username = username;
-		this.password = password;
-		this.confirmPassword = confirmPassword;
-		this.role = role;
-	}
-
+	@OneToOne(mappedBy = "users", cascade = CascadeType.ALL)
+	private Admin admin;
 }
